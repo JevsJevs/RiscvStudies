b74337490d3feae3b268a4aee34705d369ad816c
Joao Eduardo Vasconcellos <joao.eduardovsilva@hotmail.com>
2023-10-18 22:24:52 -0300

    fix(ex7.7): tests and corrections on clib functions implementations

--- Chapter7/7.7/act7.7.s
+++ Chapter7/7.7/act7.7.s
@@ -6,7 +6,16 @@ putsCharBuffer: .skip 1   #Arbitrarily set size
 getsBuffer: .skip 512
 
 .section .data
-test: .string "this is a null terminated string"
+# test: .string "this is a null terminated string"
+#TEST CASES ATOI
+# test: .string "    123af"
+test: .string "-123g"
+# test: .string "+111"
+# test: .string "121"
+# test: .string "aaa"
+# test: .string " -ho"
+#TEST CASES ITOA
+# test: .string "                                 "
 
 .section .text
 
@@ -23,29 +32,30 @@ itoa:
 #char* itoa(int val, char* str, int base)
 #This implementation assumes base = 10 || base = 16 only
 
-    mv t1, a1                       #stores str start adr
+    mv t1, a1                       #stores str absolute start adr
     li a4, 16
-    bne a3, a4, itoaBaseNot16 
+    bne a2, a4, itoaBaseNot16 
         #Sets str first digits to "0x"
         li a4, 0x30
         sb a4, 0(a1) 
-        li a4, 0x7d
+        li a4, 0x78
         sb a4, 1(a1) 
         addi a1, a1, 2
     itoaBaseNot16:
 
     li a4, 10
-    bne a3, a4, itoaBaseNot10
+    bne a2, a4, itoaBaseNot10
         bgez a0, itoaSignedNotNegative
             li a4, -1
             mul a0, a0, a4 
             li a4, 0x2d
             sb a4, 0(a1)
-            addi a4, a4, 1
+            addi a1, a1, 1
         itoaSignedNotNegative:
     itoaBaseNot10:
 
     li a5, 10
+    mv t2, a1                               #saves first digit char adr to reverse
     itoaParsingLoop:
         remu a4, a0, a2
         blt a4, a5, itoaIfDecimalDigitThen  #if remainder > 10
@@ -61,12 +71,41 @@ itoa:
         addi a1, a1, 1
     bnez a0, itoaParsingLoop
 
+    mv a0, t1                               #retrieves to a0, the string absolute start adr
+
+    addi sp, sp, -8
+    sw a1, 8(sp)
+    sw a0, 4(sp)
+    sw ra, 0(sp)
+
+    mv a0, t2
+    addi a1, a1, -1
+    jal reverseString
+    addi a1, a1, 1
+
+    lw ra, 0(sp)
+    lw a0, 4(sp)
+    lw a1, 8(sp)
+    addi sp, sp, 8
+
     sb zero, 0(a1)                          #null terminates string
-
-    #load in routine return value = str start adr
-    mv a0, t1
-
     ret    
+
+reverseString:
+# void reverseString(char* startAdr, char* endAdr)
+    reverseStringLoop:
+        blt a1, a0, reverseStringLoopEnd
+        beq a0, a1, reverseStringLoopEnd
+        lb t1, 0(a0)
+        lb t2, 0(a1)
+        sb t2, 0(a0)
+        sb t1, 0(a1)
+        addi a0, a0, 1
+        addi a1, a1, -1
+        j reverseStringLoop
+    reverseStringLoopEnd:
+
+    ret
 
 atoi:
 #int atoi(const char* str)
@@ -87,8 +126,8 @@ atoi:
         addi sp, sp, 8
 
         beqz t1, atoiOptionalSign
-        
         addi a1, a1, 1
+        j atoiWSloop
     atoiWSEndLoop:
 
     #optional plus minus
@@ -122,7 +161,7 @@ atoi:
         mul t2, t2, t3                      #For each char passed
         j atoiDigitCounterLoop
     atoiDigitCounterLoopEnd:
-    div t2, t2, 10                          #correction because extra multiplication
+    div t2, t2, t3                          #correction because extra multiplication
 
 
     li a0, 0
@@ -131,9 +170,11 @@ atoi:
         beqz t2, atoiCharToIntLoopEnd
         lb a2, 0(a1)
         addi a2, a2, -0x30
-        mul a0, a0, t2                      #val = di * pow
+        mul a2, a2, t2                      #val = di * pow
+        add a0, a0, a2
         div t2, t2, t3                      #pow = pow/10 
         addi a1, a1, 1
+        j atoiCharToIntLoop
     atoiCharToIntLoopEnd:
 
     mul a0, a0, t1
@@ -217,12 +258,12 @@ puts:
     sb a5, 0(a1)
     ecall
     
-    la a0, 0x0
+    li a0, 0x0
     ret
 
     putsError:
 
-    la a0, 0x0
+    li a0, 0x0
     ret
 
 
